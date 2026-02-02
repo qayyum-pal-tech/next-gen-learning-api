ebacfadab8828681a0c2062ffcca5baad8582db1
Qayyum Shareef <qayyum.shareef@pal.tech>
2026-02-02 13:56:44 +0530

    setup config module, mongoose and env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Next Gen Learning API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
